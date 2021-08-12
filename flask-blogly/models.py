500afb6be6e83151ba9d3b7b79a47d3aaaf05c9d
nettemarie <nettemarie@Antoinettes-MacBook-Pro.local>
2021-08-12 10:43:37 -0400

    app now includes the option to create posts

--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -1,10 +1,10 @@
-"""Models for Blogly."""
-
 """SQLAlchemy models for blogly."""
 
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
@@ -19,6 +19,8 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         """Return full name of user."""
@@ -26,10 +28,28 @@ class User(db.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
 def connect_db(app):
     """Connect this database to provided Flask app.
 
-    Call this in the Flask app.
+    You should call this in your Flask app.
     """
 
     db.app = app
