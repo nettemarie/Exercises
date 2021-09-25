bcb9e1d1c02814e6213011524822e4a29d6a4e54
nettemarie <nettemarie@Antoinettes-MBP.lan>
2021-09-25 19:33:15 -0400

    update test file

--- flask-blogly/test_app.py
+++ flask-blogly/test_app.py
@@ -1,101 +1,24 @@
-# from app import app
-# from models import db, connect_db, User, Post, Tag
-# from flask import Flask, session
-# from flask_sqlalchemy import SQLAlchemy
-# import unittest
-# from unittest import TestCase
+from app import app
+from flask import session
+from unittest import TestCase
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User, Post, Tag
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
+
+app.config["TESTING"] = True
+app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
 
 
-# app.config["TESTING"] = True
-# app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
-# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+class BloglyTestCase(TestCase):
+    def test_submit_user(self):
+        with app.test_client() as client:
 
+            user = {"first_name": "John", "last_name": "Jacobs"}
+            res = client.post("/users/new", data=user, follow_redirects=True)
+            html = res.get_data(as_text=True)
 
-# class BloglyTestCase(TestCase):
-
-#     app = Flask(__name__)
-#     db = SQLAlchemy(app)
-
-#     # def setUp(self):
-#     #     self.app.config["TESTING"] = True
-#     #     self.app.config["DEBUG"] = False
-#     #     self.app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
-#     #     self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#     #     self.app = self.app.test_client()
-#     #     self.db.drop_all()
-#     #     self.db.create_all()
-#     #     new_user = User(
-#     #         first_name="Antoinette",
-#     #         last_name="Medlock",
-#     #         image_url=None,
-#     #     )
-
-#     #     db.session.add(new_user)
-#     #     db.session.commit()
-
-#     def tearDown(self):
-#         user = User.query.filter_by(first_name="Tommy").first()
-#         self.db.session.remove()
-#         db.session.delete(user)
-
-#     def test_user_submit(self):
-#         with app.test_client() as client:
-#             res = client.post(
-#                 "/users/new",
-#                 data={
-#                     "first_name": "Tommy",
-#                     "last_name": "Lewis",
-#                     "image_url": "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png",
-#                 },
-#             )
-#             html = res.get_data(as_text=True)
-
-#             # user = User.query.filter_by(first_name="Tommy").first()
-#             # self.db.session.remove()
-#             # db.session.delete(user)
-
-#             self.assertEqual(res.status_code, 302)
-#             self.assertEqual(res.location, "http://localhost/users")
-#             # self.db.session.remove()
-#             # db.session.delete(user)
-#             # self.assertIn("Antoinette Medlock", html)
-
-#     def test_new_user(self):
-#         user = {
-#             "first_name": "Antoinette",
-#             "last_name": "Medlock",
-#             "image_url": "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png",
-#         }
-#         with app.test_client() as client:
-#             res = client.get(
-#                 "/users",
-#                 data=user,
-#             )
-
-#             html = res.get_data(as_text=True)
-
-#             self.assertEqual(res.status_code, 200)
-#             self.assertIn("Antoinette Medlock", html)
-#             self.db.session.remove()
-#             db.session.delete(user)
-
-#     def test_post_submit(self):
-#         with app.test_client() as client:
-#             res = client.post(
-#                 "/",
-#                 data={
-#                     "title": "Test",
-#                     "content": "Test post",
-#                     "user": "Antoinette Medlock",
-#                     "user_id": "1",
-#                 },
-#             )
-#             html = res.get_data(as_text=True)
-
-#             self.assertEqual(res.status_code, 302)
-#             self.assertIn("Test", html)
-
-
-# if __name__ == "__main__":
-#     unittest.main()
-#     app.run()
+            self.assertEqual(res.status_code, 404)
+            self.assertIn("John Jacobs", html)
